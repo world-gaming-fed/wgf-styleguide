5b977ceb75cc47acc1c81c153ea25261f8309778
vincentk <vincent.kocupyr@gmail.com>
2016-03-10 16:41:00 +0100

    ftr(component): menuLabel

--- src/components/elements/index.js
+++ src/components/elements/index.js
@@ -2,9 +2,10 @@ module.exports = {
   buttons: require('./buttons'),
   links: require('./links'),
   labels: require('./labels'),
-
+  MenuLabel: require('./menuLabel/MenuLabel'),
   Avatar: require('./avatar/Avatar'),
   field: require('./field/Field'),
   Icon: require('./icon/Icon'),
   TableList: require('./tableList/TableList')
+
 };
