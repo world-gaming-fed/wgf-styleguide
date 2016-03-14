4ca13ecc6fbe7ac743e9852b6c484ce9406bbe47
Damien Berseron (Hikkyu) <damien.berseron@wgf.gg>
2016-03-14 11:35:53 +0100

    add icons

--- src/base/fonts/fontIcons.styleguide.js
+++ src/base/fonts/fontIcons.styleguide.js
@@ -12,13 +12,29 @@ export default class extends React.Component {
 
   render () {
     return (
-      <p className="sg-icon-list">
-        <span className="wgf-icon-arrows_plus"></span>
-        <span className="wgf-icon-arrows_right"></span>
-        <span className="wgf-icon-twitter"></span>
-        <span className="wgf-icon-facebook"></span>
-        <span className="wgf-icon-loupe"></span>
-      </p>
+      <div>
+        <h1 className="Title">WGF Custom</h1>
+        <p className="sg-icon-list">
+          <span className="wgf-icon-loupe" title="loupe"></span>
+          <span className="wgf-icon-home" title="home"></span>
+          <span className="wgf-icon-gamepad" title="gamepad"></span>
+          <span className="wgf-icon-tickets" title="tickets"></span>
+          <span className="wgf-icon-map" title="map"></span>
+          <span className="wgf-icon-flask" title="flask"></span>
+        </p>
+
+        <h1 className="Title">Linea</h1>
+        <p className="sg-icon-list">
+          <span className="wgf-icon-arrows_plus" title="arrows_plus"></span>
+          <span className="wgf-icon-arrows_right" title="arrows_right"></span>
+        </p>
+
+        <h1 className="Title">Social</h1>
+        <p className="sg-icon-list">
+          <span className="wgf-icon-twitter" title="twitter"></span>
+          <span className="wgf-icon-facebook" title="facebook"></span>
+        </p>
+      </div>
     );
   }
 }
