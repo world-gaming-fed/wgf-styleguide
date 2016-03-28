66fe9f8d54be3d38f643d6a09898b86c896dd586
Damien Berseron (Hikkyu) <damien.berseron@wgf.gg>
2016-03-28 20:17:27 +0200

    ftr(component): add table field container and login field

--- src/components/elements/buttons/button/Button.js
+++ src/components/elements/buttons/button/Button.js
@@ -1,4 +1,6 @@
 var Link = require('react-router').Link;
+var classnames = require('classnames');
+var Icon = require('../../icon/Icon');
 
 module.exports = function(React) {
   return React.createClass({
@@ -11,28 +13,41 @@ module.exports = function(React) {
       className: React.PropTypes.string,
       href: React.PropTypes.string,
       to: React.PropTypes.string,
-      icon: React.PropTypes.string
+      icon: React.PropTypes.string,
+      /**
+       * Modifier: `twitter|facebook|callToAction|pushForward`
+       */
+      modifier: React.PropTypes.string
+    },
+
+    applyModifier: function(classes) {
+      if (this.props.modifier) {
+        classes.push(this.props.modifier.split(' ').map(function(modifier) {
+          return 'Button--' + modifier;
+        }));
+      }
+      return classes;
     },
 
     renderIcon: function() {
-      var iconClassName;
       if (this.props.icon) {
-        iconClassName = 'wgf-icon-' + this.props.icon;
-        return (<span className={iconClassName}></span>);
+        return (<Icon>{this.props.icon}</Icon>);
       }
       return null;
     },
 
     render: function() {
-      var classString = 'Button';
+      var classes = [ 'Button' ];
+
+      classes = this.applyModifier(classes);
 
       if (this.props.className) {
-        classString += ' ' + this.props.className;
+        classes.push(this.props.className);
       }
 
       if (this.props.to) {
         return (
-          <Link {...this.props} className={classString}>
+          <Link {...this.props} className={classnames(classes)}>
             {this.renderIcon()}
             <span className="Button__text">{this.props.children}</span>
           </Link>
@@ -41,7 +56,7 @@ module.exports = function(React) {
 
       if (this.props.href) {
         return (
-          <a {...this.props} className={classString}>
+          <a {...this.props} className={classnames(classes)}>
             {this.renderIcon()}
             <span className="Button__text">{this.props.children}</span>
           </a>
@@ -49,7 +64,7 @@ module.exports = function(React) {
       }
 
       return (
-        <button {...this.props} className={classString}>
+        <button {...this.props} className={classnames(classes)}>
           {this.renderIcon()}
           <span className="Button__text">{this.props.children}</span>
         </button>
