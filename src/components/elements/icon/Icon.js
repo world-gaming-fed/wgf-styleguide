e95728a310badf4db215f21b173325ab0e88511d
vincentk <vincent.kocupyr@gmail.com>
2016-05-06 17:24:03 +0200

    ftr(button): update buttons and add loader

--- src/components/elements/icon/Icon.js
+++ src/components/elements/icon/Icon.js
@@ -6,12 +6,16 @@ var classNames = require('classnames');
 var Icon = React.createClass({
   displayName: 'Icon',
   propTypes: {
-    children: React.PropTypes.string.isRequired
+    children: React.PropTypes.string.isRequired,
+    className: React.PropTypes.string
   },
   render: function() {
-    var classes = 'wgf-icon-';
+    var classes = ['wgf-icon-'];
     if (this.props.children) {
-      classes += this.props.children;
+      classes[0] += this.props.children;
+    }
+    if (this.props.className) {
+      classes.push(this.props.className);
     }
     return (
       <span className={classNames(classes)}></span>
