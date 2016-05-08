05356139a39755ad3daff0e175a591b94279c69f
Damien Berseron (Hikkyu) <damien.berseron@wgf.gg>
2016-05-09 01:57:30 +0200

    fix(helper/cpt): change helper name, add GameCard 2x image support

--- src/components/blocks/cards/gameCard/GameCard.js
+++ src/components/blocks/cards/gameCard/GameCard.js
@@ -4,20 +4,27 @@ var classnames = require('classnames');
 var GameCard = React.createClass({
   propTypes: {
     imgURL: React.PropTypes.string,
+    imgURL2x: React.PropTypes.string,
     name: React.PropTypes.string,
     size: React.PropTypes.string
   },
 
   render: function() {
-    var classes = [ {
-      GameCard: true
-    } ];
+    var classes = [ 'GameCard' ];
+    var cover2x = null;
+
     if (this.props.size) {
       classes.push('GameCard--' + this.props.size);
     }
+
+    if (this.props.imgURL2x) {
+      cover2x = (<div className="GameCard__cover" style={{backgroundImage: '-webkit-image-set(url("' + this.props.imgURL + '") 1x, url("' + this.props.imgURL2x + '") 2x)'}}></div>);
+    }
+
     return (
       <div className={classnames(classes)}>
         <div className="GameCard__cover" style={{backgroundImage: 'url(' + this.props.imgURL + ')'}}></div>
+        {cover2x}
         <div className="GameCard__mask">
           <div className="GameCard__title"><h2 className="Title">{this.props.name}</h2></div>
         </div>
