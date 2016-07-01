f8f60bbfb18f49e4cdf4e785e8512b80a6d6e913
vincentk <vincent.kocupyr@gmail.com>
2016-07-01 18:52:36 +0200

    ftr(videocard): add viewers props

--- src/components/blocks/cards/videoCard/VideoCard.js
+++ src/components/blocks/cards/videoCard/VideoCard.js
@@ -6,21 +6,34 @@ var VideoCard = React.createClass({
   propTypes: {
     imgURL: React.PropTypes.string,
     name: React.PropTypes.string,
+    viewers: React.PropTypes.string,
     modifier: React.PropTypes.string
   },
 
   render: function() {
     var classes = [ 'VideoCard' ];
-
+    var renderViewers = null;
     if (this.props.modifier) {
       classes.push('VideoCard--' + this.props.modifier);
+    }
+
+    if(this.props.viewers) {
+      renderViewers = (
+        <div className="VideoCard__text__viewers">
+          <Icon>basic_eye</Icon>
+          {this.props.viewers}
+        </div>
+      );
     }
 
     return (
       <div className={classnames(classes)}>
         <div className="VideoCard__cover" style={ {backgroundImage: 'url(' + this.props.imgURL + ')'} }/>
         <div className="VideoCard__mask" />
-        <div className="VideoCard__title"><h2 className="Title">{this.props.name}</h2></div>
+        <div className="VideoCard__text">
+          <h2 className="Title">{this.props.name}</h2>
+          {renderViewers}
+        </div>
         <div className="VideoCard__play">
           <Icon>music_play_button</Icon>
         </div>
--- src/components/blocks/cards/videoCard/VideoCard.styleguide.js
+++ src/components/blocks/cards/videoCard/VideoCard.styleguide.js
@@ -8,7 +8,7 @@ export default class extends React.Component {
     category: 'Blocks',
     title: 'VideoCard',
     description: '...',
-    code: `<VideoCard name="Game Name" imgURL="/assets/img/no-cover.jpg" />`,
+    code: `<VideoCard name="Game Name" viewers="3528" imgURL="/assets/img/no-cover.jpg" />`,
     exampleComponent: VideoCard
   }
 
@@ -18,6 +18,7 @@ export default class extends React.Component {
         <VideoCard name="Game Name" imgURL="/assets/img/no-cover.jpg" />
         <br />
         <VideoCard name="Game Name" modifier="highlight" imgURL="/assets/img/no-cover.jpg" />
+        <VideoCard name="Final Fantasy XV" viewers="15342" modifier="highlight" imgURL="/assets/img/no-cover.jpg" />
       </div>
     );
   }
