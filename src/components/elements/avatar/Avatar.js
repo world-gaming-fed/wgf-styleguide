7c8a673bb3c1a0cdd65e6b11c7f040e9ae300757
vincentk <vincent.kocupyr@gmail.com>
2016-09-08 12:31:39 +0200

    fix(svg): fix svg on firefox

--- src/components/elements/avatar/Avatar.js
+++ src/components/elements/avatar/Avatar.js
@@ -22,23 +22,41 @@ var Avatar = React.createClass({
     size: React.PropTypes.oneOf([ 'small', 'medium', 'large', 'x-large' ])
   },
 
-  renderImg: function() {
+  renderImg: function(style) {
     var img = (
-      <img className="Avatar__image" src={this.props.url} />
+      <img className="Avatar__image" src={this.props.url} style={style} />
     );
 
     if (this.props.url2x) {
       img = (
-        <img className="Avatar__image" src={this.props.url} srcSet={this.props.url2x + ' 2x'} />
+        <img className="Avatar__image" src={this.props.url} srcSet={this.props.url2x + ' 2x'} style={style} />
       );
     }
 
     return img;
   },
 
+  renderSvg: function() {
+    if (this.props.format !== 'org') {
+      return null;
+    }
+    return (
+      <svg height="0">
+        <defs>
+          <clipPath id="clipShape" clipPathUnits="objectBoundingBox">
+            <polygon points="0.5 0.019, 0.084 0.26, 0.084 0.74, 0.5 0.98, 0.91 0.74, 0.91 0.26, 0.5 0.019" />
+          </clipPath>
+        </defs>
+      </svg>
+    );
+  },
+
   render: function() {
     var classes = [ 'Avatar' ];
     var {format, size, ...other} = this.props;
+    var style = {
+      clipPath: format === 'org' ? 'url(#clipShape)' : ''
+    };
 
     if (format) {
       classes.push('Avatar--' + format);
@@ -54,7 +72,8 @@ var Avatar = React.createClass({
 
     return (
       <div className={classnames(classes)}>
-        { this.renderImg() }
+        { this.renderImg(style) }
+        { this.renderSvg() }
       </div>
     );
   }
