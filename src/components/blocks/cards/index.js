62e35d63a7e679027a11da220133efd352b261a4
Damien Berseron (Hikkyu) <damien.berseron@wgf.gg>
2016-11-03 16:51:51 +0100

    ftr(steamcard): new steam card

--- src/components/blocks/cards/index.js
+++ src/components/blocks/cards/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   EventCard: require('./eventCard/EventCard'),
   GameCard: require('./gameCard/GameCard'),
+  SteamGameCard: require('./steamGameCard/SteamGameCard'),
   GameCardAdd: require('./gameCardAdd/GameCardAdd'),
   MoreCard: require('./moreCard/MoreCard'),
   VideoCard: require('./videoCard/VideoCard')
